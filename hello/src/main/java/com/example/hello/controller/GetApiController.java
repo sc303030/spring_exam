181403fe884a530ecc13a43eb599dfbfb248c012
GyeongHyeon <lkh6035@naver.com>
2021-07-27 05:43:39 +0900

    21.07.27 | Param

--- hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -17,10 +20,25 @@ public class GetApiController {
     }
 
     // localhost:9090/api/get/path-variable/{name}
-    @GetMapping("/path-variable")
-    public String pathVariable(@PathVariable String name){
-        System.out.println("PathVariable : " + name);
-        return name;
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable(name="name") String pathName){
+        System.out.println("PathVariable : " + pathName);
+        return pathName;
+    }
 
+    // 주소 뒤에 ?key=value&key2=value2
+    // http://localhost:9090/api/get/query-param?user=steve&email=steve@email.com&age=30
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach(entry-> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey() + "=" + entry.getValue() +"\n");
+        });
+
+        return sb.toString();
     }
 }
