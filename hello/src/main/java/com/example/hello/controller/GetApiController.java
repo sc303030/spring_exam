42388c33434e48bed96d3812db1b834a3cbb405f
GyeongHyeon <lkh6035@naver.com>
2021-07-28 05:45:11 +0900

    21.07.28 | get-param

--- hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,5 +1,6 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,5 +41,28 @@ public class GetApiController {
         });
 
         return sb.toString();
+    }
+
+    @GetMapping("query-param02")
+    public String queryParam02(
+        @RequestParam String name,
+        @RequestParam String email,
+        @RequestParam int age
+
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " "+email +" " +age;
+    }
+
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
     }
 }
---
+++ hello/src/main/java/com/example/hello/dto/UserRequest.java
@@ -0,0 +1,43 @@
+package com.example.hello.dto;
+
+public class UserRequest {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String email;
+    private int age;
+
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
