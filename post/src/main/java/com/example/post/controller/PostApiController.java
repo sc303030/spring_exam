d96de4afdacb6a196f71428c743355352d6cc850
GyeongHyeon <lkh6035@naver.com>
2021-07-30 05:56:34 +0900

    21.07.30 | post

--- post/src/main/java/com/example/post/controller/PostApiController.java
+++ post/src/main/java/com/example/post/controller/PostApiController.java
@@ -1,9 +1,19 @@
 package com.example.post.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(Map<String, Object> requestData){
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
 }
